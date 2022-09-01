187758bb4b14510899edb3bc123bb89249b9f653
Tom R Baker <trbaker@gmail.com>
2022-09-01 12:42:02 -0500

    Update computer1.py

--- computer1.py
+++ computer1.py
@@ -1,6 +1,6 @@
 import serial
 import requests
-import json
+import ast
 import time
 
 ser = serial.Serial('/dev/cu.usbmodem143201', 115200, timeout=None)
@@ -15,14 +15,18 @@ while True:
     print(tempf)
     # send data to sample ArcGIS Online service
     url = 'https://services3.arcgis.com/GzteEaZqBuJ6GIYr/arcgis/rest/services/survey123_910b6ea1c50743269a5b171a91fe6cc7_fieldworker/FeatureServer/0/addFeatures'
+    #template dictionary that we'll replace item in, before sending to AGO
     params={"f":"pjson","token":"","rollbackOnFailure":"false","features":'{ \
         "attributes" : { \
             "controller" : "ACPE microcontroller sensor 1", \
             "lat" : 45.23434, \
             "lon": -94.2342, \
-            "temperature" : 111.11, \
+            "temperature" : tempf, \
             "humidity" : 0}}'
             }
-    requests.post(url, params=params)
+    params=str(params).replace("tempf", str(tempf))
+    # converts the string to a python dictionary
+    params=ast.literal_eval(params)
+    requests.post(url, params)
     time.sleep(2)       # integer is seconds to pause
     print('  resetting....')
